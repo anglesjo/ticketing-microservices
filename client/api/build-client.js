ad3b71dc54dcbce2cb086120fa2b11ca4897f1c9
Jorge Angles <anglesjo@users.noreply.github.com>
2022-09-09 17:07:22 +0200

    Final touches before DNS

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ticketing-microservices.shop',
       headers: req.headers,
     });
   } else {
