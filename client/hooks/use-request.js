1c7e4abbfd2ed29a37423a61ec9638db8e2c2013
Jorge Angles <anglesjo@users.noreply.github.com>
2022-09-10 07:52:01 +0200

    Fixed useRequest hook

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body, onSuccess }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async (props = {}) => {
@@ -15,7 +15,6 @@ export default ({ url, method, body, onSuccess }) => {
 
       return response.data;
     } catch (err) {
-      console.log(err);
       setErrors(
         <div className="alert alert-danger">
           <h4>Ooops....</h4>
@@ -31,3 +30,5 @@ export default ({ url, method, body, onSuccess }) => {
 
   return { doRequest, errors };
 };
+
+export default useRequest;
